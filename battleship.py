22cc91dbd4b96135ef7d27ca705f3a4f9f964b9e
speedfankimo <speedfankimo@gmail.com>
2017-09-30 16:02:27 +0800

    Update battleship.py
    4 times to guess where is the battleship

--- battleship.py
+++ battleship.py
@@ -2,13 +2,14 @@ from random import randint
 
 board = []
 
-for x in range(0, 5):
+for x in range(5):
     board.append(["O"] * 5)
 
 def print_board(board):
     for row in board:
         print " ".join(row)
 
+print "Let's play Battleship!"
 print_board(board)
 
 def random_row(board):
@@ -16,29 +17,31 @@ def random_row(board):
 
 def random_col(board):
     return randint(0, len(board[0]) - 1)
-  
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-
-#print ship_row
-#print ship_col
-
-guess_row = int(raw_input("Guess Row:"))
-guess_col = int(raw_input("Guess Col:"))
-
 print ship_row
 print ship_col
 
-# Write your code below!
-if guess_row == ship_row and ship_col == guess_col :
-  print "Congratulations! You sank my battleship!"
-else:
-  if guess_row not in range(5) or guess_col not in range(5):
-    print "Oops, that's not even in the ocean."
-  elif board[int(guess_row)][int(guess_col)] == "X":
-    print "You guessed that one already."
-  else: 
-    print "You missed my battleship!"
-    board[int(guess_row)][int(guess_col)] = "X"
+# Everything from here on should go in your for loop!
+# Be sure to indent four spaces!
+for turn in range(4):
+  print "Turn", turn + 1 # turn 為 0~3, 
+  guess_row = int(raw_input("Guess Row:"))
+  guess_col = int(raw_input("Guess Col:"))
+  if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sunk my battleship!"
+  else:
+    if (guess_row < 0 or guess_row > 4) \
+    or (guess_col < 0 or guess_col > 4):
+      print "Oops, that's not even in the ocean."
+    elif(board[guess_row][guess_col] == "X"):
+      print "You guessed that one already."
+    else:
+      print "You missed my battleship!"
+      board[guess_row][guess_col] = "X"
+    # Print (turn + 1) here!
   print_board(board)
+    
+#if turn = 3: # 3+1 = 猜第四次了
+  #print "Game Over"
