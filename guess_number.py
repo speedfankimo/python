cef2450d38375add0ae183c6ff232527107030d0
speedfankimo <speedfankimo@gmail.com>
2017-10-18 18:03:42 +0800

    Update guess_number.py
    add the second practice

--- guess_number.py
+++ guess_number.py
@@ -1,3 +1,4 @@
+# first practice
 from random import randint
 
 # Generates a number from 1 through 10 inclusive
@@ -13,3 +14,20 @@ while guesses_left>0:
   guesses_left -= 1
 else:
   print "You lose. The correct number is %s" % (random_number)
+  
+  
+# second practice
+import random
+random_num = random.randint(1,10)
+print random_num 
+
+guess_count = 0
+while guess_count<=5:
+    guess = int(raw_input("what you guess?"))
+    if random_num == guess:
+        print "you win"
+        break
+    guess_count += 1
+else:
+    print "you lose and number =" + str(random_num)
+
