2b1608afa285ecf8d365efb3bf570001bde213db
speedfankimo <speedfankimo@gmail.com>
2018-01-17 16:22:48 +0800

    Create reverse_string.py
    Revise String

---
+++ reverse_string.py
@@ -0,0 +1,22 @@
+# my way
+
+def reverse(string):
+  x = len(string)-1
+  new_word = ''
+  while x >= 0:
+    new_word += string[x] #new_word = new_word + string[x]
+    x -=1
+  return new_word
+    
+print reverse("!nohtyP")
+
+
+# Guidance
+def reverse2(text):
+    word = ""
+    l = len(text) - 1
+    while l >= 0:
+        word = word + text[l]
+        l -= 1
+    return word
+print reverse2('text')
