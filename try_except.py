0397ef8e738d49d5f4d9f44bfab8e8340efcde2b
speedfankimo <speedfankimo@gmail.com>
2018-04-26 15:16:23 +0800

    Update try_except.py
    add #import tkinter.messagebox

--- try_except.py
+++ try_except.py
@@ -1,11 +1,16 @@
 #! usr/bin/python
 # -*- coding:utf-8 -*-
 
+#import GUI module 
 from tkinter import *
+#improt messagebox (showinfo,showwarning,showeerror,askquestion,askokcancel,askyesnocancel,askretrycancel)
+import tkinter.messagebox
 
 app = Tk()
 app.title('Deliveries')
 app.geometry('750x600+500+100')
+
+
 
 # def click save and then save data to .text
 def save_data():
@@ -26,6 +31,14 @@ def save_data():
 		ex_app.title('Excption')
 		ex_app.geometry('400x70+600+300')
 		Label(ex_app, text = 'Excption : %s' % (exception), fg="red").pack()
+		# improt the messagebox.showerror
+		error_app = tkinter.messagebox.showerror('Error','Record cannot write into becasue of : %s' %(exception))
+		# improt the messagebox.askretrycancel
+		error_app = tkinter.messagebox.askretrycancel('Error','Record cannot write into becasue of : %s' %(exception))
+		if error_app == True:
+			save_data()
+		else:
+			tkinter.messagebox.showinfo('MESSAGE', 'There is something wrong, pls recheck')
 
 
 Label(app, text='服務據點(Depot):').pack()
